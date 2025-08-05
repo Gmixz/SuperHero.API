5828b82d560a0d6ec2fdadb66028f8de77b6f751
Gmixz <ros.biomed@gmail.com>
2025-08-05 16:27:01 -0400

    Add project files.

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperHero.API.Models;
+
+
+namespace SuperHero.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Models.SuperHero>>> GetSuperHeroes()
+        {
+            return new List<Models.SuperHero>
+            {
+                new Models.SuperHero
+                {
+                   Name = "Spider Man",
+                   FirstName = "Peter",
+                   LastName = "Parker",
+                   Place = "New York"
+
+                }
+            };
+        }
+    }
+}
