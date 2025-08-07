32fae9d781ce9e8c40d1d858ba8852ee49b446a8
Gmixz <ros.biomed@gmail.com>
2025-08-07 14:12:21 -0400

    Implement Create, Update and Delete in SuperHeroController

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SuperHero.API.Models;
+using Microsoft.EntityFrameworkCore;
+using SuperHero.API.Data;
 
 
 namespace SuperHero.API.Controllers
@@ -8,20 +9,63 @@ namespace SuperHero.API.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<Models.SuperHero>>> GetSuperHeroes()
         {
-            return new List<Models.SuperHero>
-            {
-                new Models.SuperHero
-                {
-                   Name = "Spider Man",
-                   FirstName = "Peter",
-                   LastName = "Parker",
-                   Place = "New York"
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
 
-                }
-            };
+        [HttpPost]
+        public async Task<ActionResult<List<Models.SuperHero>>> CreateSuperHero(Models.SuperHero hero)
+        {
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Models.SuperHero>>> UpdateSuperHero(Models.SuperHero hero)
+        {
+            var heroToUpdate = await _context.SuperHeroes.FindAsync(hero.Id);
+
+            if (heroToUpdate == null)
+            {
+                return BadRequest("Hero not found");
+            }
+
+            heroToUpdate.Name = hero.Name;
+            heroToUpdate.FirstName = hero.FirstName;
+            heroToUpdate.LastName = hero.LastName;
+            heroToUpdate.Place = hero.Place;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Models.SuperHero>>> DeleteSuperHero(int id)
+        {
+            var heroToDelete = await _context.SuperHeroes.FindAsync(id);
+
+            if (heroToDelete == null)
+            {
+                return BadRequest("Hero not found");
+            }
+
+            _context.SuperHeroes.Remove(heroToDelete);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
         }
     }
 }
