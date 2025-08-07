4fc0ac4bc77ed6152e92a990d6c724a41c3d5614
Gmixz <ros.biomed@gmail.com>
2025-08-07 12:43:47 -0400

    Add Entity Framework

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using SuperHero.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<DataContext>(options => 
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+// Enable Cross-Origin Resource Sharing
 builder.Services.AddCors(options => options.AddPolicy(name: "SuperHero",
                          policy => 
                          {
